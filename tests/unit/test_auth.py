c3262bd9f108bba7f5346ecfa516cea41295cf3f
Sebastián Mata <sebasmata41@gmail.com>
2026-02-14 20:21:57 -0400

    auth test still not passing

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,11 +1,8 @@
+from app.core.security import get_password_hash, verify_password
 
-from dotenv import load_dotenv
-from app.services.auth import get_password_hash, verify_password
-
-load_dotenv(dotenv_path=".env")
 
 def test_password_hash_and_verify():
     password = "test1234"
     hashed = get_password_hash(password)
     assert hashed != password
-    assert verify_password(password, hashed) 
+    assert verify_password(password, hashed)
