16c77bb7f0e460f6b4f7314fce33c032542f8a07
Sebastián Mata <sebasmata41@gmail.com>
2026-02-14 20:06:42 -0400

    test medical history generated

--- app/core/config.py
+++ app/core/config.py
@@ -3,22 +3,27 @@ from sqlmodel import SQLModel, create_engine, Session
 from typing import Annotated
 import os
 
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="../../.env") # defaults to .env if path not specified or just load_dotenv() from python-dotenv
+env_path = Path(__file__).resolve().parent.parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = os.getenv("SUPABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 engine = create_engine(DATABASE_URL, echo=True)
 
+
 def init_db():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
